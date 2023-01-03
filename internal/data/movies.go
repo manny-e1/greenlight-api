663aad5d0a28a8210d5092311c8b43150b7c88a6
manny-e1 <aewnetu6@gmail.com>
2023-01-03 19:54:34 +0300

    sorting

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/lib/pq"
@@ -63,13 +64,13 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 			the generated query term matches the lexemes. To continue the example, the query term
 			'the' & 'club' will match rows which contain both lexemes 'the' and 'club' .
 	*/
-	query := `
+	query := fmt.Sprintf(`
 		SELECT id, created_at, title, year, runtime, genres, version
 		FROM movies
 		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
 		AND (genres @> $2 OR $2 = '{}')
-		ORDER BY id`
-
+		ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
+	fmt.Println(query)
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
