3d6644db0a4bebbfcc7f0a137b0e1e6a884cad3a
manny-e1 <aewnetu6@gmail.com>
2023-01-05 07:35:12 +0300

    graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -87,19 +84,6 @@ func main() {
 
 	app := &application{config: cfg, logger: logger, models: data.NewModels(db)}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		ErrorLog:     log.New(logger, "0", 0),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-
-	logger.PrintInfo("Starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-	err = srv.ListenAndServe()
+	err = app.serve()
 	logger.PrintFatal(err, nil)
 }
